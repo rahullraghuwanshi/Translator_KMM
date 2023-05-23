8a1e312d862813172ac9877ab5cd50e014296cad
Rahul Raghuwanshi <champrahul202002@gmail.com>
2023-05-23 09:21:15 +0530

    :hammer: Android theme setup.

--- androidApp/src/main/java/com/rahulraghuwanshi/translator_kmm/android/MainActivity.kt
+++ androidApp/src/main/java/com/rahulraghuwanshi/translator_kmm/android/MainActivity.kt
@@ -4,22 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.rahulraghuwanshi.translator_kmm.Greeting
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
+            TranslatorTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    GreetingView(Greeting().greet())
+
                 }
             }
         }
@@ -34,7 +35,7 @@ fun GreetingView(text: String) {
 @Preview
 @Composable
 fun DefaultPreview() {
-    MyApplicationTheme {
+    TranslatorTheme {
         GreetingView("Hello, Android!")
     }
 }
